419ac55d4ec4168e4462bba2694acbf32ce16bf3
ibrahim31313 <104802896+ibrahim31313@users.noreply.github.com>
2023-03-21 20:42:53 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+//-My variables :
+let menuIcon = document.querySelector(".menu-icon")
+let aArr = Array.from(document.querySelectorAll(".ul a"))
+
+menuIcon.addEventListener("click", showHideMenu)
+aArr.forEach((a) => {
+    a.addEventListener("click", aActiveState, event)
+})
+
+
+//-The external functions :
+function showHideMenu() {
+    document.querySelector("nav").classList.toggle("active")
+}
+function aActiveState(e) {
+    // remove active class from all a 
+    aArr.forEach((a) => {
+        a.classList.remove("active")
+    })
+
+    // put the active class on the cuurent a 
+    e.currentTarget.classList.add("active")
+}
